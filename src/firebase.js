4bb6eeec4df7bf1f242dde482f600aa25a66eaac
Zizo El Zoz <eng-ziad.elsayd1722@alexu.edu.eg>
2026-03-07 13:00:40 +0200

    hide firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 // Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDCeSl1acc_F_VHI1hI2aNhu6z1suF-D6o",
-  authDomain: "rent-contract-app.firebaseapp.com",
-  projectId: "rent-contract-app",
-  storageBucket: "rent-contract-app.firebasestorage.app",
-  messagingSenderId: "975445760785",
-  appId: "1:975445760785:web:948336111b08e12a0d4ab0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
